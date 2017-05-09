a01661784a7a25c4a8192bd786b3e839dc00dc74
ztobin <ztobin@pasedfoundation.org>
2017-05-08 22:17:22 -0700

    Cleanup and readme

---
+++ Readme.md
@@ -0,0 +1,27 @@
+
+# Tobin Neighborhood Map
+## Description
+A front-end app that displays some of my go-to places for food and drink in Pasadena.  
+It uses the Google Maps API, Foursquare API, and the Knockout.js framework to do the following:
+* Display a map of Pasadena with some locations marked
+* Display the names of the locations in a list
+* Allow the user to filter locations based on the name and some associated keywords
+* Display a little info about the location when it is clicked on, either in the list or on the map
+
+## Installation
+Download this repository to install it. The necessary libraries are included
+
+## How to run the App
+
+Open index.html. The onscreen map can scroll and zoom like a regular Google Map.  
+Dragging the mouse over the locations on the list or the markers on the map will bounce the marker and display the location's name.  
+Clicking on the location, on the list or on the map, will animate the marker and display some more information about the current status of the establishment.
+
+
+## Acknowledgements
+This app was created using: 
+* [The Foursquare API](https://developer.foursquare.com/)
+* [The Google Maps API](https://developers.google.com/maps/)
+* [Knockout.js](http://knockoutjs.com/)
+## License
+This code released under the [MIT License](https://choosealicense.com/licenses/mit)
--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Initial Locations List
 var locations = [
     {
         title: 'Pizza Rev',
@@ -27,31 +28,26 @@ var locations = [
 ];
 
 
-var map;
-var markers = [];
-var largeInfowindow;
-var smallInfowindow;
-var windowTitle='';
-var windowText='';
+/*
+ * Knockout VMMV code
+ */
 
+// Defining a Knockout Location object
 var Loc = function (data) {
     this.title = ko.observable(data.title);
     this.position = ko.observable(data.position);
     this.fsVenue = ko.observable(data.fsVenue);
     this.tags = ko.observable(data.tags);
     this.markerID = ko.observable(data.markerID);
-    this.fullName = ko.computed(
-            function () {
-                return this.title() + " The Loc";
-            }, this);
 };
 
 
 var ViewModel = function () {
     var self = this;
     this.locList = ko.observableArray([]);
-
-    var markerID=0;
+    
+    // Marker ID helps link KO Loc object to GMaps API markers
+    var markerID = 0;
     locations.forEach(function (locItem) {
         locItem['markerID'] = markerID;
         self.locList.push(new Loc(locItem));
@@ -59,51 +55,55 @@ var ViewModel = function () {
     });
 
 
-    this.currentLoc = ko.observable(this.locList()[0]);
 
+    // Display selected location on map
     this.setCurrentLoc = function (clickedLoc) {
-        self.currentLoc(clickedLoc);
         populateInfoWindowByIndex(clickedLoc.markerID());
-        console.log('set current loc call');
     };
-    
+
+    // Function called during mouseover event on location list
     this.highlightLoc = function (selectedLoc) {
-//        console.log("highlight");
-//        console.log(selectedLoc.title());
-          var selectedMarker = markers[selectedLoc.markerID()];
-          populateSmallInfoWindow(selectedMarker);
+        var selectedMarker = markers[selectedLoc.markerID()];
+        populateSmallInfoWindow(selectedMarker);
     };
-    
+
+    // Function called when clicking 'Filter' button
     this.filterLocs = function (formElement) {
         var filterWord = $('#street').val().toLowerCase();
         var loctext = '';
         var markerIDs = [];
-        console.log('Filter submitted');
-        console.log(filterWord);
         self.locList.removeAll();
         locations.forEach(function (locItem) {
             loctext = (locItem.title + locItem.tags).toLowerCase();
-            console.log('Filter checking: ' +octext);
             if (loctext.includes(filterWord)) {
                 self.locList.push(new Loc(locItem));
                 markerIDs.push(locItem.markerID);
-            };
+            }
+            ;
         });
         filterMarkers(markerIDs);
-
     };
-
 
 };
 
-
-
 ko.applyBindings(new ViewModel());
 
-// GMaps
+/*
+ *  Google Maps API functions
+ */
 
+// Variables used in the Googlle Maps API functions
+var map;
+var markers = [];
+var largeInfowindow;
+var smallInfowindow;
+var windowTitle = '';
+var windowText = '';
+
+
+// Initialize Google Map
 function initMap() {
-    // Constructor creates a new map - only center and zoom are required.
+    
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 34.144408, lng: -118.117983},
         zoom: 14,
@@ -113,21 +113,16 @@ function initMap() {
     largeInfowindow = new google.maps.InfoWindow();
     smallInfowindow = new google.maps.InfoWindow();
 
-    // The following group uses the location array to create an array of markers on initialize.
     for (var i = 0; i < locations.length; i++) {
-        // Get the position from the location array.
         var position = locations[i].position;
         var title = locations[i].title;
-        // Create a marker per location, and put into markers array.
         var marker = new google.maps.Marker({
             position: position,
             title: title,
             animation: google.maps.Animation.DROP,
             id: i
         });
-        // Push the marker to our array of markers.
         markers.push(marker);
-        // Create an onclick event to open an infowindow at each marker.
         marker.addListener('click', function () {
             AJAXInfoWindow(this);
         });
@@ -135,102 +130,76 @@ function initMap() {
         marker.addListener('mouseover', function () {
             populateSmallInfoWindow(this);
         });
-
     }
     showListings();
 }
 
-// This function populates the infowindow when the marker is clicked. We'll only allow
-// one infowindow which will open at the marker that is clicked, and populate based
-// on that markers position.
-function populateInfoWindowByIndex(index){
-    console.log("index: " +index);
-    console.log('marker object: '+ markers[index]);
+// When option is chosen from list, calls this function
+function populateInfoWindowByIndex(index) {
     AJAXInfoWindow(markers[index]);
 }
+
+// Make AJAX call, then display location info on map
 function AJAXInfoWindow(marker) {
-    // Check to make sure the infowindow is not already opened on this marker.
     smallInfowindow.close();
     marker.setAnimation(google.maps.Animation.BOUNCE);
-    //AJAX Call
-    
-    
+
     var markerID = marker.id;
-    console.log("markerID: " + markerID);
     var currentlocation = locations[markerID];
-    console.log("currentlocation: "+ currentlocation);
     var fsVenue = currentlocation.fsVenue;
-    console.log("fsVenue: " + fsVenue);
     var fsPre = 'https://api.foursquare.com/v2/venues/';
-    // var fsVenue = '4d51e88d7ee1a35d31af9434';
     var fsID = '?client_id=F15PVAJ44Q5ASG1J2INQDCET4TX5HIDUXHFXV3DGVWXBEFBE';
     var fsSecret = '&client_secret=XX2K40GOPRYEORWWYGAPW2END33ZDJCVN1GBLCH5W4MA3JP5';
     var fsVer = '&v=20170309';
     var foursquareUrl = fsPre + fsVenue + fsID + fsSecret + fsVer;
 
     $.ajax({
-
         url: foursquareUrl,
         dataType: 'json',
         data: {
-
             async: true
         },
 
         success: function (data) {
-            console.log("ajax success");
             var venue = data.response.venue.name;
-            console.log(venue);
-            
             var formattedPhone = data.response.venue.contact.formattedPhone;
-            console.log(formattedPhone);
-            var description = data.response.venue.description;
-            console.log(description);
-             var status = data.response.venue.hours.status;
-             console.log(status);
+            var status = data.response.venue.hours.status;
             windowText = '<h2>' + venue + '</h2>';
-            windowText +='<p>' + status + '</p>';
-            windowText +='<p>Contact: ' + formattedPhone + '</p>';
-
+            windowText += '<p>' + status + '</p>';
+            windowText += '<p>Contact: ' + formattedPhone + '</p>';
         },
 
         error: function (jqXHR, textStatus, errorThrown) {
             windowText = 'Oops! touble connecting to FourSquare API. Please try later.';
-            console.log('getJSON request failed! ' + textStatus);
             infowindow.setContent(windowText);
             infowindow.open(map, marker);
         },
-  complete: function (jqXHR, status) {
+        
+        complete: function (jqXHR, status) {
             populateLargeInfoWindow(marker);
-  }
+        }
     });
 }
 
-function populateLargeInfoWindow(marker){
+// Display Location info on map
+function populateLargeInfoWindow(marker) {
     setTimeout(function () {
         marker.setAnimation(null);
     }, 750);
-    
-    console.log('text type: '+ typeof windowText);
-    console.log('Window Text:' + windowText);
+
     largeInfowindow.setContent(windowText);
-    
+
     if (largeInfowindow.marker !== marker) {
-        console.log("reset window");
         largeInfowindow.marker = marker;
-        windowText = 'hello';
-//        console.log('Window Text:' + windowText);
-//        largeInfowindow.setContent('<p>'+windowText+'</p>');
         largeInfowindow.open(map, marker);
-        // Make sure the marker property is cleared if the infowindow is closed.
         largeInfowindow.addListener('closeclick', function () {
             largeInfowindow.marker = null;
         });
     }
 }
 
+// Displays mini-location window on map for help selecting locations
 function populateSmallInfoWindow(marker) {
-    // Check to make sure the infowindow is not already opened on this marker.
     marker.setAnimation(google.maps.Animation.BOUNCE);
     setTimeout(function () {
         marker.setAnimation(null);
@@ -239,26 +208,18 @@ function populateSmallInfoWindow(marker) {
         smallInfowindow.marker = marker;
         smallInfowindow.setContent('<p>' + marker.title + '</p>');
         smallInfowindow.open(map, marker);
-        // Make sure the marker property is cleared if the infowindow is closed.
         smallInfowindow.addListener('mouseout', function () {
             smallInfowindow.close();
         });
     }
 }
 
-
-
-// This function will loop through the markers array and display them all.
-
-function filterMarkers(markerIDs){
-    console.log('filterMarkers function');
-    console.log('markerIDs: ' + markerIDs);
-    // Extend the boundaries of the map for each marker and display the marker
+// Filters out markers based on Filter
+function filterMarkers(markerIDs) {
     for (var i = 0; i < markers.length; i++) {
         markers[i].setMap(null);
     }
     var bounds = new google.maps.LatLngBounds();
-    // Extend the boundaries of the map for each marker and display the marker
     for (var i = 0; i < markerIDs.length; i++) {
         markerID = markerIDs[i];
         markers[markerID].setMap(map);
@@ -267,9 +228,9 @@ function filterMarkers(markerIDs){
     map.fitBounds(bounds);
 }
 
+// Displays markers on map
 function showListings() {
     var bounds = new google.maps.LatLngBounds();
-    // Extend the boundaries of the map for each marker and display the marker
     for (var i = 0; i < markers.length; i++) {
         markers[i].setMap(map);
         bounds.extend(markers[i].position);
@@ -277,169 +238,3 @@ function showListings() {
     map.fitBounds(bounds);
 }
 
-
-// Foursquare
-$('#show-listings').click(function () {
-    var fsPre = 'https://api.foursquare.com/v2/venues/';
-    var fsVenue = '4d51e88d7ee1a35d31af9434';
-    var fsID = '?client_id=F15PVAJ44Q5ASG1J2INQDCET4TX5HIDUXHFXV3DGVWXBEFBE';
-    var fsSecret = '&client_secret=XX2K40GOPRYEORWWYGAPW2END33ZDJCVN1GBLCH5W4MA3JP5';
-    var fsVer = '&v=20170309';
-    var foursquareUrl = fsPre + fsVenue + fsID + fsSecret + fsVer;
-    $.ajax({
-
-        url: foursquareUrl,
-        dataType: 'json',
-        data: {
-
-            async: true
-        },
-
-        success: function (data) {
-            var venue = data.response.venue.name;
-            console.log('AJAX returns: ')
-            console.log(venue);
-            var formattedPhone = data.response.venue.contact.formattedPhone;
-            console.log(formattedPhone);
-            var description = data.response.venue.description;
-            console.log(description);
-            var status = data.response.venue.hours.status;
-            console.log(status);
-
-        },
-
-        error: function (jqXHR, textStatus, errorThrown) {
-            var infoContent = '<h3>Oops! Something seems to be wrong. Please try later.';
-            console.log('getJSON request failed! ' + textStatus);
-            infowindow.setContent(infoContent);
-            infowindow.open(map, marker);
-        }
-    });
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Weather Attempt
-var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?zip=91107,us&appid=15cc9da0b27b48d6b6fa81f700733c89&units=imperial';
-console.log('ajax call');
-$.getJSON(weatherUrl, function (data) {
-    temp = data.main.temp;
-    console.log(temp + 'F');
-}).error(function (e) {
-    console.log('no weather');
-});
-
-
-
-
-
-
-
-
-
-
-//----------------------------------------------------------
-
-//APIS
-
-
-
-//
-//
-//
-// $('#form-container').submit(loadData);
-//
-//
-
-// clear out old data before new request
-function loadData() {
-    $wikiElem.text("");
-    $nytElem.text("");
-
-    var streetStr = $('#street').val();
-    var cityStr = $('#city').val();
-    var address = streetStr + ', ' + cityStr;
-
-    $greeting.text('So, you want to live at ' + address + '?');
-
-// load streetview
-    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
-    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
-
-
-
-
-// load nytimes
-// obviously, replace all the "X"s with your own API key
-    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
-    $.getJSON(nytimesUrl, function (data) {
-
-        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
-
-        articles = data.response.docs;
-        for (var i = 0; i < articles.length; i++) {
-            var article = articles[i];
-            $nytElem.append('<li class="article">' +
-                    '<a href="' + article.web_url + '">' + article.headline.main + '</a>' +
-                    '<p>' + article.snippet + '</p>' +
-                    '</li>');
-        }
-        ;
-
-    }).error(function (e) {
-        $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
-    });
-
-
-
-// load wikipedia data
-    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
-    var wikiRequestTimeout = setTimeout(function () {
-        $wikiElem.text("failed to get wikipedia resources");
-    }, 8000);
-
-    $.ajax({
-        url: wikiUrl,
-        dataType: "jsonp",
-        jsonp: "callback",
-        success: function (response) {
-            var articleList = response[1];
-
-            for (var i = 0; i < articleList.length; i++) {
-                articleStr = articleList[i];
-                var url = 'http://en.wikipedia.org/wiki/' + articleStr;
-                $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
-            }
-            ;
-
-            clearTimeout(wikiRequestTimeout);
-        }
-    });
-
-    return false;
-}
-;
-
-//---------------------------------------------------------
